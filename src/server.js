a449b7191a76415e71a28ebf0fb3c840f3ccf630
mateocarruce <mateocarruce@gmail.com>
2025-02-06 22:15:16 -0500

    Modificacion leve a server prueba 3

--- src/server.js
+++ src/server.js
@@ -114,7 +114,7 @@ sequelize.sync() // Sin forzar la recreación
     .then(() => {
         console.log('Database synced!');
         server.listen({ port: 4000 }).then(({ url }) => {
-            console.log(`🚀 Server ready owo at ${url}`);
+            console.log(`🚀 Server ready efe at ${url}`);
         });
     })
     .catch(err => {
